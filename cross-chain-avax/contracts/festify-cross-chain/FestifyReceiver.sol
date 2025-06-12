306cacdfb4d5aa1061c4f9378879568bf2b13382
Ibrahim Abdulkarim <aliveibi080@gmail.com>
2025-06-12 16:33:01 +0100

    Feat: Update contracts

--- cross-chain-avax/contracts/festify-cross-chain/FestifyReceiver.sol
+++ cross-chain-avax/contracts/festify-cross-chain/FestifyReceiver.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.24;
+pragma solidity 0.8.25;
 
 import {Client} from "@chainlink/contracts-ccip/contracts/libraries/Client.sol";
 import {CCIPReceiver} from "@chainlink/contracts-ccip/contracts/applications/CCIPReceiver.sol";
